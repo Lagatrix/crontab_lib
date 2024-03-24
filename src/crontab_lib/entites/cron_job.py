d4172dea345c9e1f06ea1356773f70d3edaff144
javierfh03 <pachi030919@gmai.com>
2024-03-24 12:00:54 +0100

    Params order in cronjob entity

--- src/crontab_lib/entites/cron_job.py
+++ src/crontab_lib/entites/cron_job.py
@@ -7,19 +7,19 @@ class CronJob:
     """This class represents a cron job.
 
     Attributes:
-        command: str: The command to be executed.
         minute: str: The minute the command will be executed.
         hour: str: The hour the command will be executed.
         day_of_month: str: The day of the month the command will be executed.
         month: str: The month the command will be executed.
         day_of_week: str: The day of the week the command will be executed.
+        command: str: The command to be executed.
     """
-    command: str
     minute: str = "*"
     hour: str = "*"
     day_of_month: str = "*"
     month: str = "*"
     day_of_week: str = "*"
+    command: str = ""
 
     def format_to_use_in_sed(self) -> str:
         """This method returns the string representation of the cron job to be used in the sed command.
